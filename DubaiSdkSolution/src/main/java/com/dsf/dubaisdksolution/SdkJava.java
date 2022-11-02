3da75b3757333c786c466945500e515890529dd4
dilipgupta123-123 <GDIlip10#$%>
2022-11-02 20:11:09 +0530

    second add activity

---
+++ DubaiSdkSolution/src/main/java/com/dsf/dubaisdksolution/SdkJava.java
@@ -0,0 +1,14 @@
+package com.dsf.dubaisdksolution;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SdkJava extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sdk_java);
+    }
+}
